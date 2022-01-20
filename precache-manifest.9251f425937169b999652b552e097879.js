0e6e47c2e82f3103d33cc252a56fb307d0033538
rachaeltay <rtay989@gmail.com>
2022-01-20 20:52:34 +0800

    Updates

---
+++ precache-manifest.9251f425937169b999652b552e097879.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913942ecb6b91ef7a7c1bd5378dddaa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "057c87427f516c697e3c",
+    "url": "/static/css/main.b5792bd0.chunk.css"
+  },
+  {
+    "revision": "2c53a1b348fa4819848b",
+    "url": "/static/js/2.070c2028.chunk.js"
+  },
+  {
+    "revision": "62eb35b2960595e5a318b3e9053bb96c",
+    "url": "/static/js/2.070c2028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057c87427f516c697e3c",
+    "url": "/static/js/main.b37aa25c.chunk.js"
+  },
+  {
+    "revision": "e16251361c5f31c799c5",
+    "url": "/static/js/runtime-main.3ffa03c4.js"
+  },
+  {
+    "revision": "f25dc81dd45203848bf5bade69c3e00c",
+    "url": "/static/media/grab_icon.f25dc81d.png"
+  },
+  {
+    "revision": "aa8209a4d29ef324bc8015dc5bf44a14",
+    "url": "/static/media/mha_icon.aa8209a4.png"
+  },
+  {
+    "revision": "2851355297fe83dc082c7eb304d9459c",
+    "url": "/static/media/profilepic.28513552.png"
+  },
+  {
+    "revision": "741ae37ee38403161ddae2ba19879e6a",
+    "url": "/static/media/us2_icon.741ae37e.png"
+  }
+]);
